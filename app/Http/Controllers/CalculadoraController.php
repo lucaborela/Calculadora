89e84262807c9e68991c69054dfb97245d69a8cb
Luca Borela <lucaborela@acad.ifma.edu.br>
2021-03-25 01:43:07 -0300

    calculadora em laravel

---
+++ app/Http/Controllers/CalculadoraController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CalculadoraController extends Controller
+{
+   public function soma()
+   {
+      return view ('soma');
+   }
+   
+}
---
+++ resources/views/soma.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link rel = "preconnect" href = "https://fonts.gstatic.com">
+<link href = "https://fonts.googleapis.com/css2? family = Noto + Sans + HK: wght @ 700 & display = swap "rel =" stylesheet ">
+</head>
+<style> 
+body{
+    família da fonte: 'Noto Sans HK', sans-serif;
+}
+</style>
+<body>
+<div class="container" >
+      
+       <div class="row mt-2">
+         cabeçalho
+          </div>
+
+       <div class="row mt-2">
+        <H1>Calculadora</H1>git 
+        <div class="row">
+        <form>
+  <div class="mb-3">
+    <label for="primeiroValor" class="form-label">Primeiro valor</label>
+    <input type="number" class="form-control" id="primeiroValor" >
+   
+  </div>
+  <div class="mb-3">
+    <label for="segundoValor" class="form-label">Segundo valor</label>
+    <input type="number" class="form-control" id="segundoValor" >
+   
+  </div>
+ 
+  
+  <button type="submit" class="btn btn-primary">Somar</button>
+</form>
+        
+
+        </div>
+        
+           </div>
+         <div class="row mt-2">
+        rodapé
+           </div>
+
+
+</div>
+  
+</body>
+</html>
